9de3ea48eea076b12531684771331a6e93bc51f7
BrianGustafBirahy <s22110506@student.unklab.ac.id>
2024-02-21 11:20:07 +0800

    menambahkan exercise 5

--- index.js
+++ index.js
@@ -4,6 +4,36 @@ const app = express()
 const user = require('./users')
 const port = 3000
 const host = '127.0.0.1'
+const multer = require("multer");
+const upload = multer({dest: "public"});
+const path = require("path");
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fs = require("fs")
+
+
+// menangani cors dengan middleware
+app.use(cors())
+
+// menangani file upload dengan middleware
+app.post("/upload", upload.single("file"), (req,res)=>{
+    const file = req.file;
+    if(file) {
+        const target = path.join(__dirname, "public", file.originalname);
+        fs.renameSync(file.path, target)
+        res.send("file berhasil diupload");
+    }
+    else {
+        res.send("file gagal diupload")
+    }
+});
+
+// menangani file statis dengan menggunakan middleware
+app.use(express.static(path.join(__dirname, "public")));
+
+// menangani Request Body dengan middleware
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 app.use(morgan("combined"));
 
@@ -11,9 +41,9 @@ app.get('/users', (req, res) => {
     res.status(200).json(user.users);
   });
 
-app.get('/users/:name', (req, res) => {
+  app.get('/users/:name', (req, res) => {
     const name = req.params.name.toLowerCase();
-    const user = users.find(u => u.name.toLowerCase() === name);
+    const user = user.users.find(u => u.name.toLowerCase() === name); // perbaiki ini
     if (!user) {
         res.status(404).json({ message: 'Data user tidak ditemukan' });
     } else {
